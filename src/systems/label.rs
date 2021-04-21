13ba205508352dbe3cbb2c1321e1c262d4cf9e43
James T <github.jtara1@gmail.com>
2021-04-20 19:30:20 -0700

    feat: add random displacement (35 to left or right) to clone placement relative to the player

--- src/systems/label.rs
+++ src/systems/label.rs
@@ -9,12 +9,17 @@ pub fn update_labels(
     mut query: Query<(&mut Text, &LabelType)>,
 ) {
     for (mut text, label_type) in query.iter_mut() {
-        text.sections[1].value = "42.42".to_string();
+        // text.sections[1].value = "42.42".to_string();
 
         match label_type {
             LabelType::FPS => {
+                // println!("found label type");
                 if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+                    // println!("found fps");
+
                     if let Some(average) = fps.average() {
+                        // println!("found fps average");
+
                         text.sections[1].value = format!("{:.0}", average);
                     }
                 }
